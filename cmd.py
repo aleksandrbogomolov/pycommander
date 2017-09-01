21dcb56a18660a6c0846a12d13164f660e002d80
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2017-09-01 23:23:28 +0300

    Formated files.

--- cmd.py
+++ cmd.py
@@ -3,11 +3,13 @@ import os
 from shutil import copyfile, move
 import util
 
+
 def listfile(path):
     """List file"""
     files = os.listdir(path)
     for file in files:
         print file
+
 
 def changedirectory(currentfolder, path):
     """Changed directory"""
@@ -15,12 +17,13 @@ def changedirectory(currentfolder, path):
         return currentfolder[0:currentfolder.rindex('/')]
     if path == '':
         return util.getproperties('rootFolder')
-    else:
-        return currentfolder + '/' + path
+    return currentfolder + '/' + path
+
 
 def copy(src, dst):
     """Copy file"""
     copyfile(src, dst)
+
 
 def movefile(src, dst):
     """Move file"""
--- util.py
+++ util.py
@@ -2,11 +2,13 @@
 import configparser
 import time
 
+
 def printgreting(username, folder):
     """Print gretings string"""
     print '======================================================='
     print '%s in %s [%s]' % (username, folder, time.strftime("%X"))
     print '======================================================='
+
 
 def getproperties(key):
     """Getting properties from properties.ini file"""
